a1d7a14cd5b3330e23b46b8e8b8b38f3e3bdbae9
La Minh Duc <laminhduc200@gmail.com>
2024-12-11 14:47:26 +0700

    Add bat script to run debug

---
+++ script/put.bat
@@ -0,0 +1,21 @@
+@echo off
+:: Set default port to COM8
+set "port=COM8"
+
+:: Check if the user provided a port argument
+if not "%2"=="" (
+    set "port=%2"
+)
+
+:: Run the ampy command with the provided or default port
+echo Using port: %port%
+ampy --port %port% put MicroPython\LoRa\LoRa.py
+ampy --port %port% put MicroPython\LoRa\message.py
+ampy --port %port% put MicroPython\LoRa\config.txt
+
+:: Run program
+if "%1"=="config" (
+    ampy --port %port% run MicroPython\LoRa\configLoRa.py
+) else (
+    ampy --port %port% run MicroPython\LoRa\main.py
+)
